50e5f60b09ec84e4d6ecfc9b3b3a5257d1f672eb
BLumia <wzc782970009@qq.com>
2015-04-14 23:45:01 +0800

    移除部分文件
    使得hustoj从主分支中分离到单独的分支，master分支进行新oj的开发

--- web/template/bs3/oj-footer.php
+++
@@ -1,8 +0,0 @@
-<span class=center>
-<HR style="FILTER: alpha(opacity=100,finishopacity=0,style=3)" width="80%" color=#987cb9 SIZE=3>
-<div align="center">
-	<p>Anything about this OnlineJudge, Please Contact <a href="mailto:<?php echo $OJ_ADMIN?>">BLumia</a></p>
-	<p></a> OJ system based on <a href='https://github.com/zhblue/hustoj'>HUSTOJ Project</a> , UI based on <a href='http://getbootstrap.com/'>Tweeter Bootstrap</p></a>
-	<p>Copyright 2015 BLumia & Vincent</p>
-</div>
-</span>
--- web/web/template/bs3/js.php
+++
@@ -1,7 +0,0 @@
-<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
-<script src="<?php echo "template/$OJ_TEMPLATE/"?>jquery.min.js"></script>
-
-<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-<script src="<?php echo "template/$OJ_TEMPLATE/"?>bootstrap.min.js"></script>
-
-
