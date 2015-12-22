729cb6e7d4301f1c0f01c826d1e2fb2c9f4a2692
BLumia <wzc782970009@qq.com>
2015-12-23 00:01:51 +0800

    变更题目的隐藏状态

--- webframe/admin/pages/problem_list.php
+++ webframe/admin/pages/problem_list.php
@@ -29,11 +29,11 @@
 					
 					$prob_defunct = $row['defunct'] == "N" ? 3 : 1;
 					$text_defunct = $row['defunct'] == "N" ? "<span class='label label-success'>Avalible</span>" : "<span class='label label-default'>Hidden</span>";
-					$url_defunct = "<a href='../api/prob_state.php?nid={$row['problem_id']}&do={$prob_defunct}'>{$text_defunct}</a>"; 
+					$url_defunct = "<a href='../api/prob_state.php?pid={$row['problem_id']}&do={$prob_defunct}'>{$text_defunct}</a>"; 
 					
 					echo "<tr>";
 					echo "<td>".$row['problem_id']."</td>";
-					echo "<td>".$row['title']."</td>";
+					echo "<td><a href='../problem.php?pid={$row['problem_id']}'>".$row['title']."</a></td>";
 					echo "<td>".$row['in_date']."</td>";
 					echo "<td>{$url_defunct}</td>";
 					echo "<td><a href='./problem_editor.php?nid=".$row['problem_id']."'>Edit</a></td>";
---
+++ webframe/api/prob_state.php
@@ -0,0 +1,58 @@
+<?php
+	session_start();
+	$ON_ADMIN_PAGE="Yap";
+	require_once("../include/setting_oj.inc.php");
+	require_once("../include/file_functions.php");
+    
+	if (!isset($_GET['pid'])) {
+		echo "Not Got an Problem Id";
+		exit(0);
+	}
+	
+	if (!isset($_GET['do'])) {
+		echo "Not Got an News Id";
+		exit(0);
+	}
+	
+	$problem_do		= intval($_GET['do']);
+	$problem_id		= intval($_GET['pid']);
+	
+	/*
+	removexss
+	if (get_magic_quotes_gpc ()) {
+		$user_id= stripslashes ( $user_id);
+		$password= stripslashes ( $password);
+	}*/
+	
+	/* do check work*/
+	$problem_id = intval($problem_id);
+	$sql=$pdo->prepare("SELECT * FROM `problem` WHERE `problem_id`=?");
+	$sql->execute(array($problem_id));
+	$affected_rows = $sql->rowCount();
+	$news_exist = ($affected_rows == 1) ? true : false;
+	
+	if($news_exist) {
+		
+		switch($problem_do) {
+			case 1:
+				$problem_defunct = "N";
+				break;
+			case 3:
+				$problem_defunct = "Y";
+				break;
+			default:
+				$problem_defunct = "N";
+				break;
+		}
+		
+		$sql=$pdo->prepare("UPDATE `problem` set `defunct`=? WHERE `problem_id`=?");
+		$sql->execute(array($problem_defunct,$problem_id));
+		echo "Problem State Modified Successful";
+		
+	} else {
+		echo "Problem NOT Exist!";
+		exit(0);
+	}
+	
+	
+?>
