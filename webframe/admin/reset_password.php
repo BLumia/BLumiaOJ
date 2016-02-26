2faf41003914d55c9c029f965a7093890af75cd5
BLumia <wzc782970009@qq.com>
2016-02-26 18:28:12 +0800

    权限检查

--- webframe/admin/reset_password.php
+++ webframe/admin/reset_password.php
@@ -9,6 +9,13 @@
 <?php
 	//Vars
 	require_once('../include/setting_oj.inc.php');
+	require_once("../include/user_check_functions.php");
+	
+	//Privilege Check
+	if (!havePrivilege("USER_MANAGER")) {
+		echo "403";
+		exit(403);
+	}
 	//Prepares
 	//Page Includes
 	require("./pages/reset_password.php");
