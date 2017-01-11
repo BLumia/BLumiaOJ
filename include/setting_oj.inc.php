331cac7809adaacda4bd6a8c452c98f9ad1790f7
Gary Wang <wzc782970009@gmail.com>
2017-01-11 17:18:07 +0800

    Config file and templete

--- include/setting_oj.inc.php
+++ include/setting_oj.inc.php
@@ -1,63 +1,22 @@
 <?php
-// OJ Info (sample examples all without "[" and "]")
-	$OJ_NAME = "BLumiaOJ"; // Name of this OJ, e.g. [BLumiaOJ] , [BLOJ]
-	$OJ_HUSTOJ_COMPATIBLE = true; // If you haven't upgrade database to BLumiaOJ, set this to true.
-	$OJ_LANGUAGE = "schinese"; // Check out language folder to know what to fill.
-	
-// Data Path Setting
-	$OJ_PROBLEM_DATA = "../../Archives/problems"; //Path to problem data floder. e.g. [/home/judge/data], this path will NOT work IF you are running on SAE or OpenShift
-	$OJ_UPLOAD_DATA = "/var/www/html/BLumiaOJ/webframe/imguploads/"; // Any file or image will be upload here. e.g. [/var/www/html/BLumiaOJ/webframe/imguploads/]
-	$OJ_WWW_UPLOAD_PATH = "./imguploads/"; // Img can be visit at http://your.site/path/to/folder/ , u should fill [path/to/folder]
-	
-// Submit Setting
-	$OJ_SUBMIT_DELTATIME = 10; // allowed submit frequence. (seconds)
-	$OJ_LANGMASK = 4080; //1mC 2mCPP 4mPascal 8mJava 16mRuby 32mBash 4080 for security reason to mask all other language
-	
-// Page Setting
-	$PAGE_ITEMS = 25;// Show how many comments/posts in one pages?
-	
-// Solution Setting
-	$SOLUTION_WA_INFO = true; // Show WA result compare?
-	$SOLUTION_SHARE = false; // Can people view the source if he/she Accepted that problem?
-	
-// Contest Setting
-	$OJ_LOCKRANK = false; // Default Lock Ranklist Mod
-	$OJ_LOCKRANK_PERCENT = 0.2; // 0~1. eg. 0.2: a 5 hours contest will lock one hour.
-	
-// Virtual Judge Setting
-	$VJ_ENABLED = false; // Currently not finished. Don't enable it unless you gonna hacking this part.
-
-// Run Enviroment and DB setting
-/* ********************
-All Supported Enviroments($ENV_CASE, aka. Data Source): 
-	"STD_MYSQL"  	You should setting the following stuff to connect to sql
-	"OPEN_SHIFT" 	Supposed by Red Hat
-	"SAE"			Supposed by Sina App Engine
-You should modify the PDO statement in setting_db.inc.php if you are not using mysql.
-******************** */
-	$ENV_CASE = "STD_MYSQL";//Environment flag, Normally should be "STD_MYSQL"
-	// If you are using STD_MYSQL, fill the following informations
-	$SQL_DB_NAME = "judge";	//Your DB Name
-	$SQL_DB_HOST = "localhost";//Your DB Host
-	$SQL_DB_PORT = "3307";//Your DB Host
-	$SQL_DB_USER = "root";//Your DB Login Username
-	$SQL_DB_PASS = "usbw";//Your DB Management Password
-	
-// Developer Setting
-	$DEV_DISPLAY_ERRORS = false; // Display Error Messages of PHP display_errors
-	
-	
-// 普通用户无需在意本分割线下面的代码
 	$OJ_PATH = dirname(dirname(__FILE__));
-
-	$OJ_PROBLEM_DATA = ($ENV_CASE == "OPEN_SHIFT") ? $_ENV['$OPENSHIFT_REPO_DIR'] : $OJ_PROBLEM_DATA;
-	if (isset($ON_ADMIN_PAGE) && $ON_ADMIN_PAGE==="Yap") {
-		require("../include/setting_db.inc.php");
-		require("../language/{$OJ_LANGUAGE}.inc.php");
+	
+	$OJ_IS_SAMPLE_CFG = null;
+	if (file_exists("{$OJ_PATH}/include/config.php")) {
+		require_once("{$OJ_PATH}/include/config.php");
+		$OJ_IS_SAMPLE_CFG = false;
+	} else if (file_exists("{$OJ_PATH}/include/config.sample.php")) { 
+		require_once("{$OJ_PATH}/include/config.sample.php");
+		$OJ_IS_SAMPLE_CFG = true;
 	} else {
-		require("./include/setting_db.inc.php");
-		require("./language/{$OJ_LANGUAGE}.inc.php");
+		exit("Error: Missing Configure file. Check your <b>OJ_PATH/include/config.php</b>");
 	}
+	
+	$OJ_PROBLEM_DATA = ($ENV_CASE == "OPEN_SHIFT") ? $_ENV['$OPENSHIFT_REPO_DIR'] : $OJ_PROBLEM_DATA;
+
+	require("{$OJ_PATH}/include/setting_db.inc.php");
+	require("{$OJ_PATH}/language/{$OJ_LANGUAGE}.inc.php");
+
 	if ($DEV_DISPLAY_ERRORS)
 		ini_set("display_errors","Off");
 	date_default_timezone_set("PRC");
