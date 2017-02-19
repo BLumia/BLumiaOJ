499b8e4fd4ac8c98c6fe1182f2050e0712c9e96a
Gary Wang <wzc782970009@gmail.com>
2017-02-19 20:50:20 +0800

    Add LICENSE

--- discuss/discuss-footer.php
+++
@@ -1,4 +0,0 @@
-<div class="footer bg-blue-light">
-&nbsp;&nbsp;Original Code by <a href="https://github.com/zhblue/hustoj">HUSTOJ</a>::Discuss, Modified by BLumia<br/>
-&nbsp;&nbsp;Stand-alone Online Judge Discuss Plug-in for <a href="https://github.com/zhblue/hustoj">HUSTOJ</a> and <a href="https://github.com/BLumia/BLumiaOJ">BLumiaOJ</a>.
-</div>
